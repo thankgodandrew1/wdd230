a32a2f40d39635a25b62c5dc08e8fab89c92b15d
thankgodandrew1 <and22046@byui.edu>
2022-10-27 18:13:33 -0600

    fixed bug in discover page

--- chamber/js/home.js
+++ chamber/js/home.js
@@ -2,7 +2,24 @@ function toggleMenu() {
     document.querySelector('.toggle').classList.toggle('open')
     document.getElementById('hamburgerBtn').classList.toggle('open')
 }
+// Last Visit section using local storage to store users last visits
+const visitsDisplay=  document.querySelector('.visits');
 
+//get the stored value in local storage
+let numVisits = Number(window.localStorage.getItem('visits-ls'));
+ 
+// determine if this is the first visit or display the number of visits
+if (numVisits !== 0){
+    visitsDisplay.textContent = numVisits;}
+    else{
+        visitsDisplay.textContent = ` Welcome 😊 this is your first visit!`;
+    }
+
+// increment the number of visits
+numVisits++;
+
+// Store the new number of visits value
+localStorage.setItem('visits-ls', numVisits);
 const x = document.getElementById('hamburgerBtn')
 x.onclick = toggleMenu;
 
