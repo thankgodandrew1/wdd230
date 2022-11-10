18127c7b78d92590fcccbbf2e48b3a5d8b195026
thankgodandrew1 <and22046@byui.edu>
2022-11-10 11:28:58 -0700

    fixed page error

--- chamber/js/directory.js
+++ chamber/js/directory.js
@@ -42,10 +42,8 @@ const gridbutton = document.querySelector("#grid");
 const listbutton = document.querySelector("#list");
 const display = document.querySelector(".directory");
 
-// The following code could be written cleaner. How? We may have to simplfiy our HTMl and think about a default view.
 
 gridbutton.addEventListener("click", () => {
-	// example using arrow function
 	display.classList.add("grid");
 	display.classList.remove("list");
 });
