37f254e4b6068d1315ab91876234f70bfba5ffde
thankgodandrew1 <and22046@byui.edu>
2022-12-04 15:12:09 -0700

    complete home page

--- wdd230_final_project/bountiful_foods/js/home.js
+++ wdd230_final_project/bountiful_foods/js/home.js
@@ -21,46 +21,63 @@ function date(){
 
 //$$$$$$$$$$$$ The Carlsbard Weather Functions section $$$$$$$$$$$$$$$
 const url = "https://api.openweathermap.org/data/2.5/weather?q=Carlsbad,US&appid=a2812664bebf2b97b52c7942dbdeb2ed&units=metric";
-// console.log(url)
-async function getWeather(url) {
+const forcastURL = "https://api.openweathermap.org/data/2.5/onecall?lat=33.1581&lon=-117.3506&exclude=current,minutely,hourly,alerts&appid=0f8c88146a435b8db9d6af1cacbbc02a&units=METRIC";
+
+async function getWeather(url, curWea) {
     const response = await fetch(url);
     const data = await response.json();
-      doStuff(data);
+    displayResult(data, curWea);
     }
-
+    
 function getWeatherIcon(icon){
     let icon_url = `https://openweathermap.org/img/wn/${icon}@2x.png`;
     return icon_url;
 };
 
-function doStuff(data) {
-    results = data;
-    let condition = `${results.weather[0].description}`;
-    condition = `${condition.replace(/(^\w{1})|(\s+\w{1})/g, letter => letter.toUpperCase())}`;
-    let icon = `${results.weather[0].icon}`;
-    let weatherIcon = `${getWeatherIcon(icon)}`;
-    let temp = `Actual Temperature: ${results.main.temp.toFixed(0)}°C`;
-    let feelsLike = `Feels Like: ${results.main.feels_like}°C`;
-    let humidity = `Humidity: ${results.main.humidity}%`;
-    let windChill = `${calcWindChill(results.main.temp, results.wind.speed)}`;
-    
-    document.querySelector(".weatherIcon").src = weatherIcon
-    document.querySelector(".weatherIcon").alt = condition
-    document.querySelector(".skyCondition").textContent = condition
-    document.querySelector("#temperature").textContent = temp
-    document.querySelector("#feelsLike").textContent = feelsLike
-    document.querySelector("#humidity").textContent = humidity
-    document.querySelector("#windChill").textContent = windChill
-}
+function displayResult(data, current = 'curWea') {
+    if(current == 'curWea'){
+        let condition = `${data.weather[0].description}`;
+        condition = `${condition.replace(/(^\w{1})|(\s+\w{1})/g, letter => letter.toUpperCase())}`;
+        let icon = `${data.weather[0].icon}`;
+        let weatherIcon = `${getWeatherIcon(icon)}`;
+        let temp = `Temperature: ${data.main.temp.toFixed(0)}°C`;
+        // let feelsLike = `Feels Like: ${data.main.feels_like}°C`;
+        let humidity = `Humidity: ${data.main.humidity}%`;
+        // let windChill = `${calcWindChill(results.main.temp, results.wind.speed)}`;
+        
+        document.querySelector(".weatherIcon").src = weatherIcon
+        document.querySelector(".weatherIcon").alt = condition
+        document.querySelector(".skyCondition").textContent = condition
+        document.querySelector("#temperature").textContent = temp
+        // document.querySelector("#feelsLike").textContent = feelsLike
+        document.querySelector("#humidity").textContent = humidity
+        // document.querySelector("#windChill").textContent = windChill
+    }
+    else{
+        const weekday = ['Sunday', 'Monday', 'Tuesday', 'Wednesday', 'Thursday', 'Friday', 'Saturday'];
 
-function calcWindChill(t, s) {
-    if (t <= 50 && s > 3){
-        const wind_chill = Math.round(
-            35.74 + 0.6215 * t - 35.75 * s **0.16 + 0.4275 * t * s ** 0.16);
-        return `Wind Chill: ${wind_chill}°C`;
-    } else {
-        return `Wind Chill: N/A`;
-    };
-  };
-getWeather(url);
-date()
+        const date = new Date();
+        const today = date.getDay();
+
+        const temp1 = data.daily[0].temp.max.toFixed(0);
+        const temp2 = data.daily[1].temp.max.toFixed(0);
+        const temp3 = data.daily[2].temp.max.toFixed(0);
+
+        const day1 = document.querySelector(".day1");
+        day1.textContent = weekday[today+1];
+        const day2 = document.querySelector(".day2");
+        day2.textContent = weekday[today+2];
+        const day3 = document.querySelector(".day3");
+        day3.textContent = weekday[today+3];
+
+        const temperature1 = document.getElementById("temp1");
+        temperature1.textContent = temp1 + "°C";
+        const temperature2 = document.getElementById("temp2");
+        temperature2.textContent = temp2 +"°C";
+        const temperature3 = document.getElementById("temp3");
+        temperature3.textContent = temp3 +"°C";
+    }
+}
+date();
+getWeather(url, 'curWea');
+getWeather(forcastURL, 'forecast');
